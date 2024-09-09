f8e60d0ad2a74e0889a46cc60726fcfd7da0c169
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-09-09 20:43:39 +0530

    Added error boundary

--- src/components/Routing/Routing.jsx
+++ src/components/Routing/Routing.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import MainLayout from "../../pages/Layout";
 import PageLoader from "../PageLoader/PageLoader";
+import CustomErrorBoundary from "../CustomErrorBoundary/CustomErrorBoundary";
 
 const Home = lazy(() => import('../../pages/Home.'));
 const CoinDetailsPage = lazy(() => import('../../pages/CoinDetailsPage'));
@@ -9,23 +10,25 @@ const CoinDetailsPage = lazy(() => import('../../pages/CoinDetailsPage'));
 function Routing(){
 
     return(
-        <Routes>
-            <Route path="/" element={<MainLayout />} >
 
-            <Route index element={
-                    <Suspense fallback={<PageLoader />}>
-                        <Home />
-                    </Suspense>
-                } />
-                <Route path="/details/:coinId" element={
+        <CustomErrorBoundary>
+            <Routes>
+                <Route path="/" element={<MainLayout />} >
+                    <Route index element={
+                            <Suspense fallback={<PageLoader />}>
+                                <Home />
+                            </Suspense>
+                        } />
+                        <Route path="/details/:coinId" element={
 
-                    <Suspense fallback={<PageLoader />}>
-                        <CoinDetailsPage />
-                    </Suspense>
+                            <Suspense fallback={<PageLoader />}>
+                                <CoinDetailsPage />
+                            </Suspense>
 
-                } />
-            </Route>
-        </Routes>
+                        } />
+                    </Route>
+            </Routes>
+        </CustomErrorBoundary>
     )
 }
 
