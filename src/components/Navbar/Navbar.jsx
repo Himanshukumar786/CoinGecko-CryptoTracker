a4612e73310249830fc2cd27247a96c5c5e0c24c
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-10-19 19:32:56 +0530

    Fix: nav bar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -17,7 +17,7 @@ function Navbar( ){
         <div className="navbar bg-base-100">
             <div className="navbar-start">
                 <div className="dropdown">
-                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+                {/* <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
                     <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="h-5 w-5"
@@ -30,7 +30,7 @@ function Navbar( ){
                         strokeWidth="2"
                         d="M4 6h16M4 12h16M4 18h7" />
                     </svg>
-                </div>
+                </div> */}
                 <ul
                     tabIndex={0}
                     className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
@@ -57,7 +57,7 @@ function Navbar( ){
                     d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                 </svg>
                 </button>
-                <button className="btn btn-ghost btn-circle">
+                {/* <button className="btn btn-ghost btn-circle">
                 <div className="indicator">
                     <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -73,7 +73,7 @@ function Navbar( ){
                     </svg>
                     <span className="badge badge-xs badge-primary indicator-item"></span>
                 </div>
-                </button>
+                </button> */}
             </div>
         </div>
     )
