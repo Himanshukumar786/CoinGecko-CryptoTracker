fd62a96e6dc80f9c466545f075f2388739a56fa6
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-10-20 23:17:02 +0530

    Fix: Navbar currency setter

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -17,7 +17,7 @@ function Navbar( ){
         <div className="navbar bg-base-100">
             <div className="navbar-start">
                 <div className="dropdown">
-                {/* <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
                     <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="h-5 w-5"
@@ -30,7 +30,7 @@ function Navbar( ){
                         strokeWidth="2"
                         d="M4 6h16M4 12h16M4 18h7" />
                     </svg>
-                </div> */}
+                </div>
                 <ul
                     tabIndex={0}
                     className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
